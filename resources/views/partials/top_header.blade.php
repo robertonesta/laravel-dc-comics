54ce454d1d224a6c884ec4291f0151f09a4a693a
RobertoNesta <robertonesta1993@gmail.com>
2023-05-29 19:11:13 +0200

    change color on Admin anchor tag

--- resources/views/partials/top_header.blade.php
+++ resources/views/partials/top_header.blade.php
@@ -1,6 +1,6 @@
 <div class="top_header bg-primary text-light py-1">
         <div class="container d-flex justify-content-between align-items-center">
-            <a class="btn btn-dark" href="{{route('comics.index')}}">Admin</a>
+            <a class="btn btn-light" href="{{route('comics.index')}}">Admin</a>
             <div>
                 <span class="text-uppercase mx-2"> Dc Power Visa<i class="fa-regular fa-registered"></i></span>
                 <select name="sites" id="sites" class="bg-primary text-light text-uppercase border-0">
