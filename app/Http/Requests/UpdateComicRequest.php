15398ca5da6bf0b50d74d4758c948201407bbce6
RobertoNesta <robertonesta1993@gmail.com>
2023-06-01 13:17:11 +0200

    customize my own error messages

--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -33,4 +33,22 @@ class UpdateComicRequest extends FormRequest
             'sale_date' => 'required|after:1900/01/01|before:today',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo deve essere inserito',
+            'title.min' => 'Il titolo deve avere un minimo di 5 caratteri.',
+            'title.max' => 'Il titolo deve avere un massimo di 5 caratteri.',
+            'thumb.max' => 'La lunghezza dell\'immagine non può superare i 355 caratteri.',
+            'description.max' => 'La lunghezza della descrizione non può superare i 3000.',
+            'series.required' => 'La serie deve essere inserita',
+            'series.min' => 'La lunghezza della serie deve essere minimo di 5 caratteri',
+            'series.max' => 'La lunghezza della serie deve essere massimo di 100 caratteri',
+            'type.min' => 'La lunghezza del tipo deve essere minimo di 5 caratteri',
+            'type.max' => 'La lunghezza del tipo deve essere massimo di 100 caratteri',
+            'sale_date.after' => 'Non si può inserire una data antecedente al 1900-01-01',
+            'sale_date.before' => 'Non si può inserire una data successiva a quella odierna',
+        ];
+    }
 }
